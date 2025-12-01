8cd203bafcf83d5d1a169ddd5e9ede47cc476436
digitalsolution360global <ds360host@gmail.com>
2025-12-01 16:36:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,23 @@
 import "./globals.css";
 import Header from "@/components/layouts/Header";
 import Footer from "@/components/layouts/Footer";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "DentalCare",
   description: "Modern Dental Clinic Website",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Header />
-        
+
         <main className="">
           {children}
         </main>
+
         <Footer />
       </body>
     </html>
