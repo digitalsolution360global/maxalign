1e54eab6ba52a58398d1c928d7fe91366ef68878
digitalsolution360global <ds360host@gmail.com>
2026-02-23 19:04:08 +0530

    Add SEO metadata for Best Dental Clinic page
    Added metadata for SEO with descriptions for Open Graph and Twitter.

--- app/best-dental-clinic-marathahalli/page.tsx
+++ app/best-dental-clinic-marathahalli/page.tsx
@@ -1,6 +1,18 @@
 import React from "react";
 import BestDental from "./BestDental";
 
+export const metadata = {
+  description:
+    "Best dental clinic in Marathahalli providing orthodontic treatment, dental implants and advanced gum care by experienced dentists",
+  openGraph: {
+    description:
+      "Best dental clinic in Marathahalli providing orthodontic treatment, dental implants and advanced gum care by experienced dentists",
+  },
+  twitter: {
+    description:
+      "Best dental clinic in Marathahalli providing orthodontic treatment, dental implants and advanced gum care by experienced dentists",
+  }
+};
 
 export default function Page() {
   return <BestDental />;
