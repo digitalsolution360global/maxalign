519d555af6303dcf80cf258ece72e306c50619ec
digitalsolution360global <ds360host@gmail.com>
2026-02-23 18:55:01 +0530

    Add metadata for affordable dental care page
    Added metadata for SEO with a description of affordable dental care services.

--- app/affordable-dental-care/page.tsx
+++ app/affordable-dental-care/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import AffordableDental from "./AffordableDental";
 
+export const metadata = {
+  description:
+    "Affordable dental care in Bangalore offering cavity treatment, gum disease treatment and dental implants with quality and comfort",
+};
 
 export default function Page() {
   return <AffordableDental />;
